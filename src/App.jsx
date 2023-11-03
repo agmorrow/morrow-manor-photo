42fa9a4c4da449f8bcc1b32ff83b5a3be86d52ab
Austin Morrow <austinmorrowdev@gmail.com>
2023-11-02 23:32:19 -0600

    start of new layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+
 
 
 
 function App() {
-
-
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
-  )
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+    </Routes>
+  </BrowserRouter>
+
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  ArrowRightIcon,
+} from "@heroicons/react/20/solid";
+
+
+const Home = () => {
+  return (
+    <div className="bg-primary-black">
+      <div className="w-full flex-auto">
+        <div className="mx-auto max-w-7xl px-6 sm:py-24 lg:px-8 mt-14 sm:mt-0 justify-start">
+          <div className="max-w-2xl py-24">
+            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+              Front End Developer from Denver, CO
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-primary-light-gray">
+              Morrow Manor Photography
+            </p>
+
+          </div>
+        </div>
+      </div>
+
+      {/* Works */}
+
+
+      {/* Review */}
+
+
+      {/* Services */}
+ 
+
+    </div>
+  );
+};
+
+export default Home;
